ff84dbb993b64ad8dc9d6d6c7cb1be9091ce0ff2
kohrVid <kohrVid@zoho.com>
2019-11-18 06:50:31 +0000

    Split up protobuf configuration
    Have moved HealthCheck configuration into its own protobuf files in
    order to maintain a separation of concerns and keep the protobuf files
    to a reasonable length.

--- authentication.pb.go
+++ authentication.pb.go
@@ -24,115 +24,6 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 
-type HealthCheckResponse_ServingStatus int32
-
-const (
-	HealthCheckResponse_UNKNOWN         HealthCheckResponse_ServingStatus = 0
-	HealthCheckResponse_SERVING         HealthCheckResponse_ServingStatus = 1
-	HealthCheckResponse_NOT_SERVING     HealthCheckResponse_ServingStatus = 2
-	HealthCheckResponse_SERVICE_UNKNOWN HealthCheckResponse_ServingStatus = 3
-)
-
-var HealthCheckResponse_ServingStatus_name = map[int32]string{
-	0: "UNKNOWN",
-	1: "SERVING",
-	2: "NOT_SERVING",
-	3: "SERVICE_UNKNOWN",
-}
-
-var HealthCheckResponse_ServingStatus_value = map[string]int32{
-	"UNKNOWN":         0,
-	"SERVING":         1,
-	"NOT_SERVING":     2,
-	"SERVICE_UNKNOWN": 3,
-}
-
-func (x HealthCheckResponse_ServingStatus) String() string {
-	return proto.EnumName(HealthCheckResponse_ServingStatus_name, int32(x))
-}
-
-func (HealthCheckResponse_ServingStatus) EnumDescriptor() ([]byte, []int) {
-	return fileDescriptor_d0dbc99083440df2, []int{1, 0}
-}
-
-type HealthCheckRequest struct {
-	Service              string   `protobuf:"bytes,1,opt,name=service,proto3" json:"service,omitempty"`
-	XXX_NoUnkeyedLiteral struct{} `json:"-"`
-	XXX_unrecognized     []byte   `json:"-"`
-	XXX_sizecache        int32    `json:"-"`
-}
-
-func (m *HealthCheckRequest) Reset()         { *m = HealthCheckRequest{} }
-func (m *HealthCheckRequest) String() string { return proto.CompactTextString(m) }
-func (*HealthCheckRequest) ProtoMessage()    {}
-func (*HealthCheckRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_d0dbc99083440df2, []int{0}
-}
-
-func (m *HealthCheckRequest) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_HealthCheckRequest.Unmarshal(m, b)
-}
-func (m *HealthCheckRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_HealthCheckRequest.Marshal(b, m, deterministic)
-}
-func (m *HealthCheckRequest) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_HealthCheckRequest.Merge(m, src)
-}
-func (m *HealthCheckRequest) XXX_Size() int {
-	return xxx_messageInfo_HealthCheckRequest.Size(m)
-}
-func (m *HealthCheckRequest) XXX_DiscardUnknown() {
-	xxx_messageInfo_HealthCheckRequest.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_HealthCheckRequest proto.InternalMessageInfo
-
-func (m *HealthCheckRequest) GetService() string {
-	if m != nil {
-		return m.Service
-	}
-	return ""
-}
-
-type HealthCheckResponse struct {
-	Status               HealthCheckResponse_ServingStatus `protobuf:"varint,1,opt,name=status,proto3,enum=proto.HealthCheckResponse_ServingStatus" json:"status,omitempty"`
-	XXX_NoUnkeyedLiteral struct{}                          `json:"-"`
-	XXX_unrecognized     []byte                            `json:"-"`
-	XXX_sizecache        int32                             `json:"-"`
-}
-
-func (m *HealthCheckResponse) Reset()         { *m = HealthCheckResponse{} }
-func (m *HealthCheckResponse) String() string { return proto.CompactTextString(m) }
-func (*HealthCheckResponse) ProtoMessage()    {}
-func (*HealthCheckResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_d0dbc99083440df2, []int{1}
-}
-
-func (m *HealthCheckResponse) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_HealthCheckResponse.Unmarshal(m, b)
-}
-func (m *HealthCheckResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_HealthCheckResponse.Marshal(b, m, deterministic)
-}
-func (m *HealthCheckResponse) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_HealthCheckResponse.Merge(m, src)
-}
-func (m *HealthCheckResponse) XXX_Size() int {
-	return xxx_messageInfo_HealthCheckResponse.Size(m)
-}
-func (m *HealthCheckResponse) XXX_DiscardUnknown() {
-	xxx_messageInfo_HealthCheckResponse.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_HealthCheckResponse proto.InternalMessageInfo
-
-func (m *HealthCheckResponse) GetStatus() HealthCheckResponse_ServingStatus {
-	if m != nil {
-		return m.Status
-	}
-	return HealthCheckResponse_UNKNOWN
-}
-
 type AuthenticationRequest struct {
 	Username             string   `protobuf:"bytes,1,opt,name=username,proto3" json:"username,omitempty"`
 	Password             string   `protobuf:"bytes,2,opt,name=password,proto3" json:"password,omitempty"`
@@ -145,7 +36,7 @@ func (m *AuthenticationRequest) Reset()         { *m = AuthenticationRequest{} }
 func (m *AuthenticationRequest) String() string { return proto.CompactTextString(m) }
 func (*AuthenticationRequest) ProtoMessage()    {}
 func (*AuthenticationRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_d0dbc99083440df2, []int{2}
+	return fileDescriptor_d0dbc99083440df2, []int{0}
 }
 
 func (m *AuthenticationRequest) XXX_Unmarshal(b []byte) error {
@@ -191,7 +82,7 @@ func (m *AuthenticationResponse) Reset()         { *m = AuthenticationResponse{}
 func (m *AuthenticationResponse) String() string { return proto.CompactTextString(m) }
 func (*AuthenticationResponse) ProtoMessage()    {}
 func (*AuthenticationResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_d0dbc99083440df2, []int{3}
+	return fileDescriptor_d0dbc99083440df2, []int{1}
 }
 
 func (m *AuthenticationResponse) XXX_Unmarshal(b []byte) error {
@@ -220,9 +111,6 @@ func (m *AuthenticationResponse) GetResult() string {
 }
 
 func init() {
-	proto.RegisterEnum("proto.HealthCheckResponse_ServingStatus", HealthCheckResponse_ServingStatus_name, HealthCheckResponse_ServingStatus_value)
-	proto.RegisterType((*HealthCheckRequest)(nil), "proto.HealthCheckRequest")
-	proto.RegisterType((*HealthCheckResponse)(nil), "proto.HealthCheckResponse")
 	proto.RegisterType((*AuthenticationRequest)(nil), "proto.AuthenticationRequest")
 	proto.RegisterType((*AuthenticationResponse)(nil), "proto.AuthenticationResponse")
 }
@@ -230,26 +118,18 @@ func init() {
 func init() { proto.RegisterFile("authentication.proto", fileDescriptor_d0dbc99083440df2) }
 
 var fileDescriptor_d0dbc99083440df2 = []byte{
-	// 302 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x74, 0x51, 0xdd, 0x4e, 0x83, 0x30,
-	0x18, 0x95, 0x19, 0x99, 0x7e, 0x44, 0x59, 0x3a, 0x5d, 0x90, 0x68, 0x62, 0xb8, 0xda, 0x15, 0x31,
-	0xf8, 0x02, 0x1a, 0x5c, 0xd4, 0x98, 0x94, 0x04, 0xfc, 0xb9, 0x5c, 0x2a, 0x6b, 0x06, 0x11, 0x01,
-	0xdb, 0xa2, 0xaf, 0xe4, 0x63, 0x1a, 0x5a, 0x6a, 0x64, 0x61, 0x57, 0xcd, 0xe9, 0xf9, 0xce, 0xf9,
-	0x7a, 0x4e, 0xe1, 0x98, 0x34, 0x22, 0xa3, 0xa5, 0xc8, 0x53, 0x22, 0xf2, 0xaa, 0xf4, 0x6b, 0x56,
-	0x89, 0x0a, 0xed, 0xc9, 0xc3, 0xf3, 0x01, 0xdd, 0x53, 0x52, 0x88, 0x2c, 0xcc, 0x68, 0xfa, 0x1e,
-	0xd3, 0xcf, 0x86, 0x72, 0x81, 0x1c, 0x18, 0x73, 0xca, 0xbe, 0xf2, 0x94, 0x3a, 0xc6, 0x85, 0x31,
-	0x3f, 0x88, 0x35, 0xf4, 0x7e, 0x0c, 0x98, 0xf6, 0x04, 0xbc, 0xae, 0x4a, 0x4e, 0xd1, 0x35, 0x98,
-	0x5c, 0x10, 0xd1, 0x70, 0x29, 0x38, 0x0a, 0xe6, 0x6a, 0x8d, 0x3f, 0x30, 0xeb, 0x27, 0xad, 0x57,
-	0xb9, 0x4e, 0xe4, 0x7c, 0xdc, 0xe9, 0xbc, 0x08, 0x0e, 0x7b, 0x04, 0xb2, 0x60, 0xfc, 0x8c, 0x1f,
-	0x71, 0xf4, 0x8a, 0x27, 0x3b, 0x2d, 0x48, 0x16, 0xf1, 0xcb, 0x03, 0xbe, 0x9b, 0x18, 0xc8, 0x06,
-	0x0b, 0x47, 0x4f, 0x4b, 0x7d, 0x31, 0x42, 0x53, 0xb0, 0x25, 0x08, 0x17, 0x4b, 0x2d, 0xd9, 0xf5,
-	0x22, 0x38, 0xb9, 0xe9, 0x25, 0xd7, 0xe9, 0x5c, 0xd8, 0x6f, 0x38, 0x65, 0x25, 0xf9, 0xd0, 0xf1,
-	0xfe, 0x70, 0xcb, 0xd5, 0x84, 0xf3, 0xef, 0x8a, 0xad, 0x9c, 0x91, 0xe2, 0x34, 0xf6, 0x2e, 0x61,
-	0xb6, 0x69, 0xd8, 0xa5, 0x9f, 0x81, 0xc9, 0x28, 0x6f, 0x0a, 0xd1, 0xf9, 0x75, 0x28, 0xc0, 0x60,
-	0xaa, 0x02, 0xd0, 0x2d, 0x58, 0xff, 0xaa, 0x40, 0xa7, 0x43, 0xf5, 0xc8, 0xd7, 0xb9, 0xee, 0xf6,
-	0xe6, 0x82, 0xf5, 0x66, 0xa4, 0x44, 0x7d, 0x0b, 0xc2, 0x60, 0x87, 0x8c, 0xae, 0x5a, 0x82, 0x14,
-	0x6a, 0xc5, 0x59, 0xe7, 0x33, 0xd8, 0x81, 0x7b, 0xbe, 0x85, 0x55, 0x8b, 0xde, 0x4c, 0xc9, 0x5e,
-	0xfd, 0x06, 0x00, 0x00, 0xff, 0xff, 0x83, 0x88, 0x0d, 0x12, 0x3c, 0x02, 0x00, 0x00,
+	// 173 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x49, 0x2c, 0x2d, 0xc9,
+	0x48, 0xcd, 0x2b, 0xc9, 0x4c, 0x4e, 0x2c, 0xc9, 0xcc, 0xcf, 0xd3, 0x2b, 0x28, 0xca, 0x2f, 0xc9,
+	0x17, 0x62, 0x05, 0x53, 0x4a, 0xfe, 0x5c, 0xa2, 0x8e, 0x28, 0xd2, 0x41, 0xa9, 0x85, 0xa5, 0xa9,
+	0xc5, 0x25, 0x42, 0x52, 0x5c, 0x1c, 0xa5, 0xc5, 0xa9, 0x45, 0x79, 0x89, 0xb9, 0xa9, 0x12, 0x8c,
+	0x0a, 0x8c, 0x1a, 0x9c, 0x41, 0x70, 0x3e, 0x48, 0xae, 0x20, 0xb1, 0xb8, 0xb8, 0x3c, 0xbf, 0x28,
+	0x45, 0x82, 0x09, 0x22, 0x07, 0xe3, 0x2b, 0x19, 0x70, 0x89, 0xa1, 0x1b, 0x58, 0x5c, 0x90, 0x9f,
+	0x57, 0x9c, 0x2a, 0x24, 0xc6, 0xc5, 0x56, 0x94, 0x5a, 0x5c, 0x9a, 0x53, 0x02, 0x35, 0x0f, 0xca,
+	0x33, 0x4a, 0x47, 0x77, 0x42, 0x70, 0x6a, 0x51, 0x59, 0x66, 0x72, 0xaa, 0x90, 0x1f, 0x17, 0xbf,
+	0x73, 0x51, 0x6a, 0x0a, 0x48, 0x22, 0x31, 0xc7, 0x39, 0x23, 0x35, 0x39, 0x5b, 0x48, 0x06, 0xe2,
+	0x7a, 0x3d, 0xac, 0x6e, 0x96, 0x92, 0xc5, 0x21, 0x0b, 0x71, 0x40, 0x12, 0x1b, 0x58, 0xd6, 0x18,
+	0x10, 0x00, 0x00, 0xff, 0xff, 0xd3, 0x39, 0xc1, 0xa8, 0x11, 0x01, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -259,82 +139,6 @@ var _ grpc.ClientConn
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
 const _ = grpc.SupportPackageIsVersion4
-
-// HealthClient is the client API for Health service.
-//
-// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
-type HealthClient interface {
-	// If the requested service is unknown, the call will fail with status
-	// NOT_FOUND.
-	HealthCheck(ctx context.Context, in *HealthCheckRequest, opts ...grpc.CallOption) (*HealthCheckResponse, error)
-}
-
-type healthClient struct {
-	cc *grpc.ClientConn
-}
-
-func NewHealthClient(cc *grpc.ClientConn) HealthClient {
-	return &healthClient{cc}
-}
-
-func (c *healthClient) HealthCheck(ctx context.Context, in *HealthCheckRequest, opts ...grpc.CallOption) (*HealthCheckResponse, error) {
-	out := new(HealthCheckResponse)
-	err := c.cc.Invoke(ctx, "/proto.Health/HealthCheck", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-// HealthServer is the server API for Health service.
-type HealthServer interface {
-	// If the requested service is unknown, the call will fail with status
-	// NOT_FOUND.
-	HealthCheck(context.Context, *HealthCheckRequest) (*HealthCheckResponse, error)
-}
-
-// UnimplementedHealthServer can be embedded to have forward compatible implementations.
-type UnimplementedHealthServer struct {
-}
-
-func (*UnimplementedHealthServer) HealthCheck(ctx context.Context, req *HealthCheckRequest) (*HealthCheckResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method HealthCheck not implemented")
-}
-
-func RegisterHealthServer(s *grpc.Server, srv HealthServer) {
-	s.RegisterService(&_Health_serviceDesc, srv)
-}
-
-func _Health_HealthCheck_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(HealthCheckRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(HealthServer).HealthCheck(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/proto.Health/HealthCheck",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(HealthServer).HealthCheck(ctx, req.(*HealthCheckRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-var _Health_serviceDesc = grpc.ServiceDesc{
-	ServiceName: "proto.Health",
-	HandlerType: (*HealthServer)(nil),
-	Methods: []grpc.MethodDesc{
-		{
-			MethodName: "HealthCheck",
-			Handler:    _Health_HealthCheck_Handler,
-		},
-	},
-	Streams:  []grpc.StreamDesc{},
-	Metadata: "authentication.proto",
-}
 
 // AuthenticationServiceClient is the client API for AuthenticationService service.
 //
